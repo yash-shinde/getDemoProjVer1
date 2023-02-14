e226d9c970c3dec28c5203aa3d24f297e2a0ba96
Administrator <Administrator@newmedico66-pc.excelsoft.com>
2023-02-14 11:56:28 +0530

    commiting new test to local branch

--- src/gitDemoProjVer1/newTest.java
+++ src/gitDemoProjVer1/newTest.java
@@ -1,10 +1,9 @@
 package gitDemoProjVer1;
 
-public class Test1 {
+public class newTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World from tester !!!");
+		System.out.println("HELLOWORLDWOROWOWOWOWO");
 	}
-
 }
